19f50215eb778ec38d49ae2091ee1b32ada0c040
Igor Molchanov <66907532+meg4cyberc4t@users.noreply.github.com>
2024-05-05 22:22:49 +0300

    Fixes with pylint (and md a default value for mode parameter)

--- nrequests.py
+++ nrequests.py
@@ -1,11 +1,20 @@
-import aiohttp
- #
-# Copyright (c) 2024, Igor Molchanov.  Please see the AUTHORS file
-# for details. All rights reserved. Use of this source code is governed by a
-# BSD-style license that can be found in the LICENSE file.
+"""
+Network requests to LXP IThub. Return specific values or dict.
 
-# Returns the user's authorization token and id
-async def login(session: aiohttp.ClientSession, login: str, password: str) -> tuple[str, int]:
+Copyright (c) 2024, Igor Molchanov.  Please see the AUTHORS file
+for details. All rights reserved. Use of this source code is governed by a
+BSD-style license that can be found in the LICENSE file.
+"""
+
+import aiohttp
+
+
+async def sign_in(session: aiohttp.ClientSession, login: str, password: str) -> tuple[str, int]:
+    """
+    Authorization in the system.
+    
+    Returns the user's authorization token and id.
+    """
     data = {
         "login": login,
         "password": password,
@@ -16,17 +25,20 @@ async def login(session: aiohttp.ClientSession, login: str, password: str) -> tu
             message = response_json['message']
             raise RuntimeError(f"Not getting the authorization key: {message}")
         return response_json['token'], response_json['data']['id']
-        
-    
+
+
 async def subject(session: aiohttp.ClientSession, subject_id: int) -> dict:
+    """Getting a subject from LXP IThub."""
     async with session.get(f'/api/v2/subjects/{subject_id}') as response:
         response_json = await response.json()
         if not response.ok:
             message = response_json['message']
             raise RuntimeError(f"Not getting the subject: {message}")
         return response_json['data']
-    
+
+
 async def step(session: aiohttp.ClientSession, step_id: int) -> dict:
+    """Getting a step from LXP IThub."""
     async with session.get(f'/api/v2/lessons/{step_id}') as response:
         response_json = await response.json()
         if not response.ok:
@@ -34,7 +46,9 @@ async def step(session: aiohttp.ClientSession, step_id: int) -> dict:
             raise RuntimeError(f"Not getting the step: {message}")
         return response_json['data']
 
+
 async def load_file(session: aiohttp.ClientSession, link: str) -> bytes:
+    """Getting a file from LXP IThub."""
     if "https" not in link:
         async with session.get(link, allow_redirects=True) as response:
             return await response.content.read()
@@ -42,10 +56,18 @@ async def load_file(session: aiohttp.ClientSession, link: str) -> bytes:
         async with session.get(link, allow_redirects=True) as response:
             return await response.content.read()
 
-async def student_subjects_per_page(session: aiohttp.ClientSession, page: int, user_id: int) -> dict:
-    async with session.get(f'/api/v2/users/{user_id}/subjects?role=student&page={page}' ) as response:        
+
+async def student_subjects_per_page(
+    session: aiohttp.ClientSession,
+    page: int,
+    user_id: int
+) -> dict:
+    """Lazy load request to get a list of the user's subjects by student role."""
+    address = f'/api/v2/users/{user_id}/subjects?role=student&page={page}'
+    async with session.get(address) as response:
         response_json = await response.json()
         if not response.ok:
             message = response_json['message']
             raise RuntimeError(f"Not getting the subjects: {message}")
         return response_json['data']
+    
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
-# Copyright (c) 2024, Igor Molchanov.  Please see the AUTHORS file
-# for details. All rights reserved. Use of this source code is governed by a
-# BSD-style license that can be found in the LICENSE file.
+"""
+Additional functionality.
+
+Copyright (c) 2024, Igor Molchanov.  Please see the AUTHORS file
+for details. All rights reserved. Use of this source code is governed by a
+BSD-style license that can be found in the LICENSE file.
+"""
 
 import re
 import asyncio
@@ -8,10 +12,17 @@ import time
 
 
 def santize_path(source: str) -> str:
+    """Clearing a string of characters prohibited for use in a file path"""
     return re.sub(r'[^\w\-_\. ]', '_', source)
 
 
 class SequenceLimit:
+    """
+    A decorator that slows down the function call. 
+    No more often than calls_limit per period.
+    
+    Prevents 429 (Too Many Requests) network error.
+    """
     def __init__(self, calls_limit: int = 5, period: int = 1):
         self.calls_limit = calls_limit
         self.period = period
@@ -19,6 +30,7 @@ class SequenceLimit:
         self.requests_finish_time = []
 
     async def sleep(self):
+        """Waiting in the queue if the restriction is passed"""
         if len(self.requests_finish_time) >= self.calls_limit:
             sleep_before = self.requests_finish_time.pop(0)
             if sleep_before >= time.monotonic():
@@ -26,12 +38,10 @@ class SequenceLimit:
 
     def __call__(self, func):
         async def wrapper(*args, **kwargs):
-
             async with self.semaphore:
                 await self.sleep()
                 res = await func(*args, **kwargs)
                 self.requests_finish_time.append(time.monotonic() + self.period)
-
             return res
-
         return wrapper
+    
